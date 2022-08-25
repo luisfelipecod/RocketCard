9edf861ba40b97a5bb3daaf930455fc630d98c4b
Luis Felipe <luisfelipe.tbr@gmail.com>
2022-08-25 19:43:38 -0300

    Create component UserCard(Incomplete)

---
+++ src/components/UserCard/UserCard.js
@@ -0,0 +1,100 @@
+import styled from "styled-components";
+
+
+export const Wrapper = styled.div`
+    width: 438px;
+    height: 693px;
+
+    margin-inline: auto;
+    position: relative;
+
+    
+    border-inline: 15px;
+    border-block: 26px;
+    border-color: black;
+    border-style: solid;
+    border-radius: 50px;
+
+    background-color: #0E1218;
+    color: #fff;
+
+    overflow: hidden;
+`
+
+export const HeaderCard = styled.header`
+    display: flex;
+    align-items: center;
+
+    margin-top: 38px;
+    margin-left: 34px;
+
+    width: 230px;
+    height: 55px;
+
+    border: 2px solid red;
+`
+
+export const Avatar = styled.div`
+    margin-top: 32px;
+
+    width:372px;
+    height: 370px;
+
+    border:10px solid #8257E5;
+    border-radius: 50%;
+
+
+    transform: translateX(89px);
+
+    & > img{
+        width: 100%;
+        height: 100%;
+        border-radius: inherit;
+    }
+`
+
+export const InfoUserCard = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    position: absolute;
+    width: 195px;
+    height: 194px;
+    bottom: 96px;
+    left: 21px;
+
+    border-radius: 50px;
+
+    background: linear-gradient(180deg, #000000 3.65%,
+                                rgba(70, 69, 69, 0.72) 53.65%, 
+                                rgba(62, 56, 56, 0.42) 100%);
+
+    & > ul{
+        list-style: none;
+        padding: 0;
+    }
+    & > ul > li{
+        display: flex;
+        align-items: center;
+        gap: 10px;
+
+        font-size: 16px;
+        font-weight: 400;
+        line-height: 24px;
+
+    }                           
+`
+
+export const FooterCard = styled.footer`
+    display: flex;
+    align-items: center;
+    width: 184px;
+    height: 35px;
+
+    position: absolute;
+    
+    right: 7px;
+    bottom: 27px;
+    border: 2px solid red;
+`
---
+++ src/components/UserCard/index.jsx
@@ -0,0 +1,53 @@
+import {
+    Wrapper,
+    HeaderCard,
+    Avatar,
+    InfoUserCard,
+    FooterCard
+} from "./UserCard";
+
+import companyIcon from "../../assets/company.svg"
+import followersIcon from "../../assets/followers.svg"
+import followingIcon from "../../assets/following.svg"
+import locationIcon from "../../assets/location.svg"
+import repositoryIcon from "../../assets/repository.svg"
+
+export function UserCard(){
+    return (
+        <Wrapper>
+            <HeaderCard>Birobiro</HeaderCard>
+            <Avatar>
+                <img src="https://source.unsplash.com/random" alt="Uma imagem de perfil" />
+            </Avatar>
+            <InfoUserCard>
+                <ul>
+                    <li>
+                        <img src={followersIcon} alt="Icone de Seguidores" />
+                        716 Seguidores
+                    </li>
+
+                    <li>
+                        <img src={followingIcon} alt="Icone de Seguindo Pessoas" />
+                        193 Seguindo
+                    </li>
+
+                    <li>
+                        <img src={repositoryIcon} alt="Icone de Repositório" />
+                        38 Repositórios
+                    </li>
+
+                    <li>
+                        <img src={companyIcon} alt="Icone de uma Empresa" />
+                        @Rocketseat
+                    </li>
+
+                    <li>
+                        <img src={locationIcon} alt="Icone de um ponto de localização de GPS" />
+                        Bebedouro
+                    </li>
+                </ul>
+            </InfoUserCard>
+            <FooterCard>ROCKETCARD</FooterCard>
+        </Wrapper>
+    )
+}
