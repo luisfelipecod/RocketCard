401846aef0d7088b5adc09a458b1c21523d224b1
Luis Felipe <luisfelipe.tbr@gmail.com>
2022-09-02 15:08:25 -0300

    ajust in home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,11 +8,16 @@ export const Container = styled.div`
     gap: 8%;
 
     min-height:100vh;
+
+    @media (max-width:600px) {
+        gap:30px;
+    }
 `
 
 export const Title = styled.h1`
     font-size: 20px;
     line-height: 30.24px;
+    margin-bottom: 12px;
 `
 
 export const MainContainer = styled.main`
