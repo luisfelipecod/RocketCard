c7377fd17aa2fd5b803a4166aaef39829a0aa1b3
Luis Felipe <luisfelipe.tbr@gmail.com>
2022-09-02 14:04:31 -0300

    create styles folder and file global.js

---
+++ src/styles/global.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color:#0E1218;
+    color: #F1F1F1;
+    font-family: 'Prompt', sans-serif;
+  }
+`
